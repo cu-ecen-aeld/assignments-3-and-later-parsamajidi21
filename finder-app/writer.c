1a11af47150f0a8436554603663bba28e5553e4f
Parsa Majidi <parsa.majidi@studenti.unipd.it>
2023-04-17 20:18:51 +0200

    New modification of assignment 2

---
+++ finder-app/writer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <errno.h>
+#include <syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+	
+    int wf;
+    const char *buf_sf;
+    ssize_t nr;
+    wf = open(argv[1], O_RDWR | O_CREAT, 0664);
+    buf_sf = argv[2];
+    if(wf == -1){
+        perror("return Error in open(): ");
+    }else{
+        nr = write(wf, buf_sf, strlen(buf_sf));
+        if(nr == -1){
+            perror("return Errot write");
+        }
+    }
+    openlog(NULL, 0, LOG_USER);
+    syslog(LOG_DEBUG, "Writing %s to %s \n", buf_sf, argv[1]);
+    syslog(LOG_ERR, "Error has occured");
+}
