1f2d0227f40a26568476fa592e6a3870997f013c
Parsa Majidi <parsa.majidi@studenti.unipd.it>
2023-04-18 16:59:42 +0200

    assignment2

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -13,13 +13,16 @@ int main(int argc, char *argv[]){
     const char *buf_sf;
     ssize_t nr;
     wf = open(argv[1], O_RDWR | O_CREAT, 0664);
+    printf("\n");
     buf_sf = argv[2];
     if(wf == -1){
         perror("return Error in open(): ");
+	return 1;
     }else{
         nr = write(wf, buf_sf, strlen(buf_sf));
         if(nr == -1){
             perror("return Errot write");
+	    return 1;
         }
     }
     openlog(NULL, 0, LOG_USER);
