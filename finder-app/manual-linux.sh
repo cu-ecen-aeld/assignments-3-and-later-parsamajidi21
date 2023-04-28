4b369fe1c6eb4232b38e4e1df45fe3e176c09401
parsamajidi21 <117867910+parsamajidi21@users.noreply.github.com>
2023-04-28 17:27:33 +0200

    test4

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -96,9 +96,11 @@ make CONFIG_PREFIX=${OUTDIR}/rootfs\
     CROSS_COMPILE=${CROSS_COMPILE} install
 echo "Finished Busybox Make & Install"
 
+cd ${OUTDIR}/rootfs/
+
 echo "Library Dependencies"
-${CROSS_COMPILE}readelf -a /bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a /bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot -v)
