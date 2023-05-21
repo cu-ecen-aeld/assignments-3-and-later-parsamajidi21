380db70ed2697ce489ddebaec6068ed38401cd5d
parsamajidi21 <117867910+parsamajidi21@users.noreply.github.com>
2023-05-21 15:54:02 +0200

    assignment4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,8 +8,8 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
-
+#new changes for Assignment 4
+username=$(cat /etc/finder-app/conf/username.txt)
 if [ $# -lt 3 ]
 then
 	echo "Using default value ${WRITESTR} for string to write"
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -54,11 +54,11 @@ make
 
 for i in $( seq 1 $NUMFILES)
 do
-	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	sh /usr/bin/writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
-finder.sh "$WRITEDIR" "$WRITESTR" > /tmp/assignment4-result.txt
+OUTPUTSTRING=$(sh /usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
+sh /usr/bin/finder.sh "$WRITEDIR" "$WRITESTR" > /tmp/assignment4-result.txt
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
